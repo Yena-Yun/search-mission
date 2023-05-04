778c076cdf1cefee16aec4ed431d36f0cd8d1606
shaqok <shaqok8@gmail.com>
2023-05-05 00:08:30 +0900

    Refactor: Axios Instance 코드 분리
    Co-authored-by: kyhui1115 <kyhui1115@users.noreply.github.com>
    Co-authored-by: pparksang1013 <pparksang1013@users.noreply.github.com>
    Co-authored-by: 조승현 <tmdgus95@users.noreply.github.com>
    Co-authored-by: powercording <powercording@users.noreply.github.com>
    Co-authored-by: eunyeong <jjangeunyeong@users.noreply.github.com>
    Co-authored-by: 임예지 <1myeji@users.noreply.github.com>
    Co-authored-by: Sheryl Yena Yun <cheryleduart@gmail.com>
    Co-authored-by: bicco2 <bicco2@users.noreply.github.com>

---
+++ src/api/SearchApi.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const SearchApi = axios.create({
+  method: "GET",
+  baseURL: "/api/v1/search-conditions",
+});
+
+SearchApi.interceptors.request.use((config) => {
+  console.info("calling api");
+  return config;
+});
+
+SearchApi.interceptors.response.use((response) => {
+  if (response.status !== 200) {
+    throw new Error("api 호출이 실패하였습니다.");
+  }
+  if (response.data.length === 0) {
+    console.info("api 호출이 실패하였습니다.");
+  }
+
+  const url = new URLSearchParams(`${response.config.url}`);
+  const key = url.get("name");
+
+  localStorage.setItem(
+    key,
+    JSON.stringify({
+      value: response.data,
+      expiresAt: Date.now() + 1000 * 60 * 5, // 5분
+    })
+  );
+
+  return response.data;
+});
+
+export default SearchApi;
